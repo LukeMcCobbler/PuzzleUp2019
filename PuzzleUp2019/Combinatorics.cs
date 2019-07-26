23368ffd21d397387486ef22b98dea4ac453536b
Boluç Papuççuoğlu <bolucpap@gmail.com>
2019-07-26 14:57:41 +0300

    Initial commit

---
+++ PuzzleUp2019/Combinatorics.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PuzzleUp2019
+{
+    internal class Combinatorics
+    {
+        public Combinatorics()
+        {
+        }
+
+        public void GenerateAllTuples<T>(T[] alphabet,int tupleLength,Action<T[]> onTupleDone)
+        {
+            recurseTupleBuild(new T[tupleLength],0, alphabet,tupleLength,onTupleDone,new Dictionary<T,bool>(),true,(item,count,partial)=>true );
+        }
+        internal void GenerateCombinations<T>(T[] items,int comboLength,Action<T[]> onCombinationFound,Func<T,int,T[],bool> itemAddCheck )
+        {
+            recurseTupleBuild(new T[comboLength],0, items,comboLength,onCombinationFound,items.ToDictionary(itm=>itm,itm=>false) ,false,itemAddCheck);
+        }
+        private void recurseTupleBuild<T>(T[] workArea, int currentLength, T[] alphabet, int tupleLength, Action<T[]> onTupleDone,Dictionary<T,bool> usage,bool allowRepetition,Func<T,int,T[],bool> itemAddCheck )
+        {
+            if(currentLength==tupleLength)
+            {
+                onTupleDone(workArea);
+            }
+            else
+            {
+                foreach(var item in alphabet.Where(itm=>itemAddCheck(itm,currentLength,workArea) ).Where(itm=>allowRepetition|| !usage[itm]  ))
+                {
+                    workArea[currentLength]=item;
+                    usage[item]=true;
+                    recurseTupleBuild(workArea,currentLength+1,alphabet,tupleLength,onTupleDone,usage,allowRepetition,itemAddCheck);
+                    usage[item]=false;
+                }
+            }
+        }
+    }
+}
---
+++ PuzzleUp2019/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PuzzleUp2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = new Combinatorics();
+            var envelopments = new List<string>();
+            c.GenerateCombinations(new char[] { 'A', 'B', 'C' }, 3, tuple =>
+            {
+                var x = new string(tuple);
+                envelopments.Add(x);
+            },(item,count,partial)=>true);
+            //Console.WriteLine(String.Join("\r\n", envelopments));
+            c.GenerateCombinations(envelopments.ToArray(), 3, tuple =>
+            {
+                var y = string.Join(",",tuple );
+                Console.WriteLine(y);
+            },checkEnvelopePosition);
+            return;
+        }
+        private static int AChar='A';
+
+        private static bool checkEnvelopePosition(string item, int position, string[] partial)
+        {
+            if ( item[0]!= (char) AChar+position)
+            {
+                return false;
+            }
+            var doubleUse=partial.Take(position).Any(partialElement=>partialElement[1]==item[1] || partialElement[2]==item[2] );
+            return !doubleUse;
+        }
+    }
+}
