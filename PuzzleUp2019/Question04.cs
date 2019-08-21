4d696b9efc9b24a81f2314cb7b4ca83a64a5610f
Boluç Papuççuoğlu <bolucpap@gmail.com>
2019-08-21 18:09:12 +0300

    Fix for bug in adjacent digits

--- PuzzleUp2019/Question04.cs
+++ PuzzleUp2019/Question04.cs
@@ -16,22 +16,22 @@ namespace PuzzleUp2019
             var vowelStarters = new HashSet<int>() { 1, 8 };
             var consonantEnders = new HashSet<int>() { 4, 6, 7, 8 };
             var digits = Enumerable.Range(0, 10).ToArray();
-            var results=new List<int>();
+            var results = new List<int>();
             new Combinatorics().GenerateCombinations(digits, 10, result => { return; }, (addCandidate, position, partialSolution) =>
                    {
                        var candidate = new int[] { addCandidate };
-                       if (position >= 2 && !checkVowelStarters(partialSolution.Skip(2 - position).Take(2).Concat(candidate), vowelStarters))
+                       if (position >= 2 && !checkVowelStarters(partialSolution.Skip(position - 2).Take(2).Concat(candidate), vowelStarters))
                        {
                            return false;
                        }
-                       if (position >= 1 && !checkConsonantEnders(partialSolution.Skip(1 - position).Take(1).Concat(candidate), consonantEnders))
+                       if (position >= 1 && !checkConsonantEnders(partialSolution.Skip(position - 1).Take(1).Concat(candidate), consonantEnders))
                        {
                            return false;
                        }
                        var retval = position > 0 || addCandidate != 0;
                        if (retval)
                        {
-                           var writeOut=string.Join("", partialSolution.Take(position).Concat(candidate).Select(c => c.ToString()));
+                           var writeOut = string.Join("", partialSolution.Take(position).Concat(candidate).Select(c => c.ToString()));
                            //Console.WriteLine(writeOut);
                            results.Add(Int32.Parse(writeOut));
                        }
@@ -42,12 +42,12 @@ namespace PuzzleUp2019
 
         private bool checkConsonantEnders(IEnumerable<int> enumerable, HashSet<int> consonantEnders)
         {
-            return enumerable.Where(i=>consonantEnders.Contains(i)).Count()==1;
+            return enumerable.Where(i => consonantEnders.Contains(i)).Count() == 1;
         }
 
         private bool checkVowelStarters(IEnumerable<int> enumerable, HashSet<int> vowelStarters)
         {
-            return enumerable.Where(i=>vowelStarters.Contains(i)).Count()==1;
+            return enumerable.Where(i => vowelStarters.Contains(i)).Count() == 1;
         }
     }
 }
